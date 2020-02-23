a744950bec33df2bf3b067aee8cc2fc408529d4b
Ian meow meow <whatthe852852@hotmail.com>
2020-02-23 22:34:52 +0800

    some minor changes

--- src/javameowmeow/ExtractData_Abstract.java
+++ src/javameowmeow/ExtractData_Abstract.java
@@ -1,9 +1,9 @@
 package javameowmeow;
 
 abstract public class ExtractData_Abstract {
-	protected boolean AddData(DataRow dat, Database connection) //use that to add your data
+	protected boolean AddData(DataRow dat, Database_Interface connection) //use that to add your data
 	{
-		return connection.AddData(dat);
+		return ((Database)connection).AddData(dat);
 	}
 	abstract public void UpdateStomp(/*add your parameters here*/);
 	abstract public boolean UpdateReddit(int pages, int perpage);
